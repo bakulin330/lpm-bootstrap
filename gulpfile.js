9c73226b457365c475b8e018909eb5c26698b542
Aleksandr Bakulin <bakulin.a.v@yandex.ru>
2016-10-17 18:04:49 +0500

    z

--- gulpfile.js
+++ gulpfile.js
@@ -8,16 +8,12 @@
         csso = require('gulp-csso'),
         notify = require('gulp-notify'),
         gutil = require('gulp-util'),
-        jswrap = require('gulp-lpmotor-jswrap'),
-        uglify = require('gulp-uglify'),
         concat = require('gulp-concat'),
         imagemin = require('gulp-imagemin'),
-        templateCache = require('gulp-angular-templatecache'),
         htmlmin = require('gulp-htmlmin'),
         pngquant = require('imagemin-pngquant'),
         concatCss = require('gulp-concat-css'),
         sourcemaps = require('gulp-sourcemaps'),
-        ngAnnotate = require('gulp-ng-annotate'),
         jade = require("gulp-jade"),
         rigger = require("gulp-rigger"),
         debug = require("gulp-debug"),
@@ -34,20 +30,15 @@
         build: {
             base: 'build/',
             img: 'build/img/',
-            fonts: 'build/fonts/',
-            templates: 'build/modules/'
+            fonts: 'build/fonts/'
         },
         src: {
             index: 'src/index.jade',
             style: ['src/build.scss'],
-            vendor: 'src/js/vendor.js',
-            js: 'src/build.js',
             img: 'src/img/**/*.*',
-            fonts: 'src/fonts/**/*.*',
-            templates: ['src/js/modules/**/*.jade'] //'src/js/modules/**/*.jade'
+            fonts: 'src/fonts/**/*.*'
         },
         watch: {
-            js: ['src/js/**/*.js', '!src/js/vendor.js'],
             style: ['src/style/**/*.scss', 'src/js/modules/**/*.scss']
         },
         clean: './build'
@@ -71,9 +62,6 @@
 
     gulp.task('build', [
         'build:index',
-        'build:vendor',
-        'build:templates',
-        'build:js',
         'build:style',
         'build:image',
         'build:fonts'
@@ -82,57 +70,13 @@
     gulp.task('watch', function () {
         gulp.watch(path.src.index, ['build:index']);
         gulp.watch(path.watch.style, ['build:style']);
-        gulp.watch(path.src.vendor, ['build:vendor']);
-        gulp.watch(path.watch.js, ['build:js']);
         gulp.watch(path.src.img, ['build:image']);
         gulp.watch(path.src.fonts, ['build:fonts']);
-        gulp.watch(path.src.templates, ['build:templates']);
     });
 
     gulp.task('build:index', function () {
         return gulp.src(path.src.index)
             .pipe(jade())
-            .pipe(gulp.dest(path.build.base))
-            .pipe(reload({stream: true}));
-    });
-
-    gulp.task('build:vendor', function () {
-        return gulp.src(path.src.vendor)
-            //.pipe(sourcemaps.init())
-            .pipe(rigger())
-            .pipe(concat('v.js'))
-            //.pipe(sourcemaps.write())
-            .pipe(gulp.dest(path.build.base))
-            .pipe(reload({stream: true}));
-    });
-
-    gulp.task('build:templates', function () {
-        return gulp.src(path.src.templates)
-            .pipe(jade())
-            .pipe(gulp.dest(path.build.templates))
-            .pipe(reload({stream: true}));
-    });
-
-    gulp.task('build:js', function () {
-        var jsStream = gulp.src(path.src.js)
-            .pipe(rigger());
-
-        var templateHeader = 'angular.module("<%= module %>"<%= standalone %>, []).run(["$templateCache", function($templateCache) {';
-        var tplStream = gulp.src(path.src.templates)
-            .pipe(jade())
-            .pipe(htmlmin({collapseWhitespace: prod, removeComments: true}))
-            .pipe(templateCache('templates.app.js', {
-                module: 'templates.app',
-                templateHeader: templateHeader
-            }));
-
-        return jsStream // merge2(jsStream, tplStream)
-            //.pipe(sourcemaps.init())
-            .pipe(concat('b.js'))
-            .pipe(ngAnnotate())
-            .pipe(jswrap('_err(e,\'{{fn_id}}\')', ['_err']))
-            .pipe(prod || minify ? uglify().on('error', gutil.log) : gutil.noop())
-            //.pipe(sourcemaps.write())
             .pipe(gulp.dest(path.build.base))
             .pipe(reload({stream: true}));
     });
--- src/js/vendor.js
+++
@@ -1,5 +0,0 @@
-//= ../../bower_components/angular/angular.min.js
-//= ../../bower_components/angular-ui-router/release/angular-ui-router.min.js
-//= ../../bower_components/angular-cookies/angular-cookies.min.js
-//= ../../bower_components/angular-messages/angular-messages.min.js
-//= ../../bower_components/angular-modal/modal.min.js
